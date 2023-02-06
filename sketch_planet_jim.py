a95cc907f868328c200a327fe41eacd07c023990
R4chel <rae84@cornell.edu>
2023-02-06 17:06:28 -0500

    refactor

--- sketch_planet_jim.py
+++ sketch_planet_jim.py
@@ -13,14 +13,18 @@ class PlanetJimSketch(vsketch.SketchClass):
     width = vsketch.Param(5., decimals=2, unit="in")
     height = vsketch.Param(3., decimals=2, unit="in")
     pen_width = vsketch.Param(0.7, decimals=3, unit="mm")
-    num_layers = vsketch.Param(1)
-    num_steps = vsketch.Param(2)
+    num_layers = vsketch.Param(3)
+    num_steps = vsketch.Param(1000)
     edge_buffer = vsketch.Param(1, unit="px")
+    min_radius_multiplier = vsketch.Param(0.2, decimals=4)
+    max_radius_multiplier = vsketch.Param(0.95, decimals=4)
+    precision = vsketch.Param(3)
 
     # maxRadiusRatio = vsketch.Param(0.2, decimals=5)
     min_radius = vsketch.Param(0.1, decimals=4, unit="in")
 
-    max_inner_circles_ratio = vsketch.Param(2.0, decimals=3)
+    max_num_inner_circles = vsketch.Param(20)
+    max_inner_circles_ratio = vsketch.Param(0.5, decimals=3)
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size(f"{self.height}x{self.width}", landscape=True, center=False)
@@ -31,9 +35,7 @@ class PlanetJimSketch(vsketch.SketchClass):
                             min(self.width, self.height) / 2,
                             random_elem(vsk, layers))
         for i in range(self.num_steps):
-            bigCircle.spawn_inner_cirlce(vsk, self.edge_buffer,
-                                         self.min_radius, layers,
-                                         self.max_inner_circles_ratio)
+            bigCircle.spawn_inner_cirlce(vsk, self, layers)
         bigCircle.draw(vsk)
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
@@ -78,22 +80,32 @@ class MyShape:
         for shape in self.inner_shapes:
             shape.draw(vsk)
 
-    def spawn_inner_cirlce(self, vsk, edge_buffer, min_radius, layers,
-                           max_inner_circles_ratio):
-        theta = vsk.random(0, np.pi * 2)
-        max_r = self.radius / 2 - edge_buffer
-        offset = vsk.random(0, max_r)
+    def spawn_inner_cirlce(self, vsk, config, layers):
+        x = vsk.random(0, 1)
+        y = vsk.random(0, 1)
+        a = min(x, y)
+        b = max(x, y)
+        offset = b * self.radius - config.edge_buffer
+        theta = 2 * np.pi * a / b
         p = Point2D(a=theta, r=offset) + self.center
-        r = maxRadiusAtP(self.inner_shapes, p, max_r)
-        if r is None or r < min_radius:
+        r = maxRadiusAtP(
+            self.inner_shapes, p,
+            min(self.radius * config.max_inner_circles_ratio,
+                self.radius - offset))
+
+        if r is None:
             return None
 
+        r *= np.round(
+            vsk.random(config.min_radius_multiplier,
+                       config.max_radius_multiplier), config.precision)
+
+        if r < config.min_radius:
+            return None
         other_layers = [l for l in layers
                         if l != self.layer] if len(layers) > 1 else layers
         layer = random_elem(vsk, other_layers)
         newShape = MyShape(p, r, layer)
         self.inner_shapes.append(newShape)
-        maxInnerCircles = int(r * max_inner_circles_ratio)
-        for _ in range(maxInnerCircles):
-            newShape.spawn_inner_cirlce(vsk, edge_buffer, min_radius, layers,
-                                        max_inner_circles_ratio)
+        for _ in range(config.max_num_inner_circles):
+            newShape.spawn_inner_cirlce(vsk, config, layers)
